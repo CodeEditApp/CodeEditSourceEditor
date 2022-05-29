512af95b9bff41d467a4fc3865ec9aa5dc91b8ef
Lukas Pistrol <lukas@pistrol.com>
2022-05-29 13:25:44 +0200

    added tests workflow

---
+++ .github/scripts/tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ARCH=""
+    
+if [ $1 = "arm" ]
+then
+    ARCH="arm64"
+else
+    ARCH="x86_64"
+fi
+
+echo "Building with arch: ${ARCH}"
+
+export LC_CTYPE=en_US.UTF-8
+
+set -o pipefail && arch -"${ARCH}" xcodebuild  \
+           -scheme CodeEditTextView \
+           -destination "platform=macos,arch=${ARCH}" \
+           clean test | xcpretty
