ea8e4624b0d9b9e2614c77c15e3986c7d9a4facb
Elias Wahl <Eliasruben2204@gmail.com>
2023-03-05 14:44:13 +0100

    Added functionality to compute cursor position at last line

--- Sources/CodeEditTextView/STTextViewController.swift
+++ Sources/CodeEditTextView/STTextViewController.swift
@@ -371,7 +371,8 @@ public class STTextViewController: NSViewController, STTextViewDelegate, ThemeAt
         guard let textLayoutManager = textView.textLayoutManager as NSTextLayoutManager?,
               let textContentManager = textLayoutManager.textContentManager as NSTextContentManager?,
               let insertionPointLocation = textLayoutManager.insertionPointLocation,
-              let cursorTextLineFragment = textLayoutManager.textLineFragment(at: insertionPointLocation)
+              let documentStartLocation = textLayoutManager.documentRange.location as NSTextLocation?,
+              let documentEndLocation = textLayoutManager.documentRange.endLocation as NSTextLocation?
         else {
             return
         }
@@ -386,18 +387,38 @@ public class STTextViewController: NSViewController, STTextViewDelegate, ThemeAt
                 options: [.rangeNotRequired, .upstreamAffinity]
             ) { _, textSegmentFrame, _, _ -> Bool
                 in
+                var col = 1
+                /// If the cursor is at the end of the document:
+                if textLayoutManager.offset(from: insertionPointLocation, to: documentEndLocation) == 0 {
+                    /// If document is empty:
+                    if textLayoutManager.offset(from: documentStartLocation, to: documentEndLocation) == 0 {
+                        self.cursorPosition.wrappedValue = (1, 1)
+                        return false
+                    }
+                    guard let cursorTextFragment = textLayoutManager.textLayoutFragment(for: textSegmentFrame.origin),
+                          let cursorTextLineFragment = cursorTextFragment.textLineFragments.last
+                    else { return false }
 
-                /// +1, because we start with the first character with 1
-                var col = cursorTextLineFragment.characterIndex(for: textSegmentFrame.origin) + 1
+                    col = cursorTextLineFragment.characterRange.length + 1
+                    if col == 1 { line += 1 }
+                } else {
+                    guard let cursorTextLineFragment = textLayoutManager.textLineFragment(at: insertionPointLocation)
+                    else { return false }
 
-                /// If cursor is at end of line add 1:
-                if cursorTextLineFragment.characterRange.length != 1 &&
-                    (cursorTextLineFragment.typographicBounds.width == (textSegmentFrame.maxX + 5.0)) {
-                    col += 1
+                    /// +1, because we start with the first character with 1
+                    let tempCol = cursorTextLineFragment.characterIndex(for: textSegmentFrame.origin)
+                    let result = tempCol.addingReportingOverflow(1)
+
+                    if !result.overflow { col = result.partialValue }
+                    /// If cursor is at end of line add 1:
+                    if cursorTextLineFragment.characterRange.length != 1 &&
+                        (cursorTextLineFragment.typographicBounds.width == (textSegmentFrame.maxX + 5.0)) {
+                        col += 1
+                    }
+
+                    /// If cursor is at first character of line, the current line is not being included
+                    if col == 1 { line += 1 }
                 }
-
-                /// If cursor is at first character of line, the current line is not being included
-                if col == 1 { line += 1 }
 
                 self.cursorPosition.wrappedValue = (line, col)
                 return false
