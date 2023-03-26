a60580a07ea773c95b7c4d4ea29ddc7d15365145
Elias Wahl <82230675+Eliulm@users.noreply.github.com>
2023-03-26 22:54:28 +0200

    Make Ruler look more like Xcode by creating custom font. Make ruler font and ruler Inset scale with text font size. (#170)
    <!--- IMPORTANT: If this PR addresses multiple unrelated issues, it will
    be closed until separated. -->
    
    ### Description
    
    This PR adds a customized SF Pro Font which aims to look like the Xcode
    Digits font.
    The ruler font and the ruler insets scale with the text font size. 
    
    ### Related Issues
    
    <!--- REQUIRED: Tag all related issues (e.g. * #123) -->
    <!--- If this PR resolves the issue please specify (e.g. * closes #123)
    -->
    <!--- If this PR addresses multiple issues, these issues must be related
    to one other -->
    
    * closes #58
    * closes #157 
    
    ### Checklist
    
    <!--- Add things that are not yet implemented above -->
    
    - [x] I read and understood the [contributing
    guide](https://github.com/CodeEditApp/CodeEdit/blob/main/CONTRIBUTING.md)
    as well as the [code of
    conduct](https://github.com/CodeEditApp/CodeEdit/blob/main/CODE_OF_CONDUCT.md)
    - [x] The issues this PR addresses are related to each other
    - [x] My changes generate no new warnings
    - [x] My code builds and runs on my machine
    - [x] My changes are all related to the related issue above
    - [x] I documented my code
    
    ### Screenshots
    
    <!--- REQUIRED: if issue is UI related -->
    PointSize 13:
    Xcode left, CodeEdit Right:
    <img width="73" alt="image"
    src="https://user-images.githubusercontent.com/82230675/227798735-1041b4a0-7aa9-4e3e-8c73-a8993427e862.png">
    
    PointSize 20:
    Xcode left, CodeEdit Right:
    <img width="162" alt="image"
    src="https://user-images.githubusercontent.com/82230675/227799017-87c08fb7-bac5-4a63-8fb2-b0bf4b0f3f0b.png">
    
    
    
    <!--- IMPORTANT: Fill out all required fields. Otherwise we might close
    this PR temporarily -->
    
    ---------
    
    Co-authored-by: Lukas Pistrol <l.pistrol@gmail.com>

--- Sources/CodeEditTextView/Controller/STTextViewController.swift
+++ Sources/CodeEditTextView/Controller/STTextViewController.swift
@@ -130,9 +130,9 @@ public class STTextViewController: NSViewController, STTextViewDelegate, ThemeAt
         rulerView.textColor = .secondaryLabelColor
         rulerView.drawSeparator = false
         rulerView.baselineOffset = baselineOffset
-        rulerView.font = NSFont.monospacedDigitSystemFont(ofSize: 9.5, weight: .regular)
+        rulerView.font = rulerFont
         rulerView.selectedLineHighlightColor = theme.lineHighlight
-        rulerView.rulerInsets = STRulerInsets(leading: 20, trailing: 8)
+        rulerView.rulerInsets = STRulerInsets(leading: rulerFont.pointSize * 1.6, trailing: 8)
 
         if self.isEditable == false {
             rulerView.selectedLineTextColor = nil
@@ -255,6 +255,8 @@ public class STTextViewController: NSViewController, STTextViewDelegate, ThemeAt
         rulerView?.selectedLineHighlightColor = theme.lineHighlight
         rulerView?.baselineOffset = baselineOffset
         rulerView.highlightSelectedLine = isEditable
+        rulerView?.rulerInsets = STRulerInsets(leading: rulerFont.pointSize * 1.6, trailing: 8)
+        rulerView?.font = rulerFont
 
         if let scrollView = view as? NSScrollView {
             scrollView.drawsBackground = useThemeBackground
