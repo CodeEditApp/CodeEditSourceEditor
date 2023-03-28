169e4ea5b2fb2bb014286b0c236b852fc3e958bf
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2023-03-28 09:09:32 -0500

    Indent Options and Clarify Tab Width (#171)
    <!--- IMPORTANT: If this PR addresses multiple unrelated issues, it will
    be closed until separated. -->
    
    ### Description
    
    > This is a near clone of #147, but git got messed up on that branch.
    This PR improves that branch anyways.
    
    This enables configuration of the behavior when the tab key is pressed.
    Previously all tabs were converted to spaces and inserted `tabWidth`
    spaces in place of the tab character. This PR clarifies that the
    `tabWidth` parameter should be used for the *visual* width of tabs, and
    adds an `indentOption` parameter that specifies how to handle inserting
    tab characters.
    
    Adds an `IndentOption` enum with two cases for this behavior:
    - `spaces(count: Int)`
    - `tab`
    
    If `spaces(count: Int)` is specified, the editor will insert the given
    number of spaces when the tab key is pressed, otherwise the tab
    character will be kept.
    
    ### Related Issues
    
    <!--- REQUIRED: Tag all related issues (e.g. * #123) -->
    <!--- If this PR resolves the issue please specify (e.g. * closes #123)
    -->
    <!--- If this PR addresses multiple issues, these issues must be related
    to one other -->
    
    * #80 - Does not close, needs an additional PR for the tab width
    setting.
    
    ### Checklist
    
    <!--- Add things that are not yet implemented above -->
    
    - [x] I read and understood the [contributing
    guide](https://github.com/CodeEditApp/CodeEdit/blob/main/CONTRIBUTING.md)
    as well as the [code of
    conduct](https://github.com/CodeEditApp/CodeEdit/blob/main/CODE_OF_CONDUCT.md)
    - [x] The issues this PR addresses are related to each other
    - [x] My changes generate no new warnings
    - [x] My code builds and runs on my machine
    - [x] My changes are all related to the related issue above
    - [x] I documented my code
    
    ### Screenshots
    
    
    https://user-images.githubusercontent.com/35942988/228014785-85a20e2e-0465-4767-9d53-b97b4df2e11e.mov

--- Sources/CodeEditTextView/CodeEditTextView.swift
+++ Sources/CodeEditTextView/CodeEditTextView.swift
@@ -18,7 +18,8 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
     ///   - language: The language for syntax highlighting
     ///   - theme: The theme for syntax highlighting
     ///   - font: The default font
-    ///   - tabWidth: The tab width
+    ///   - tabWidth: The visual tab width in number of spaces
+    ///   - indentOption: The behavior to use when the tab key is pressed. Defaults to 4 spaces.
     ///   - lineHeight: The line height multiplier (e.g. `1.2`)
     ///   - wrapLines: Whether lines wrap to the width of the editor
     ///   - editorOverscroll: The percentage for overscroll, between 0-1 (default: `0.0`)
@@ -33,6 +34,7 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
         theme: Binding<EditorTheme>,
         font: Binding<NSFont>,
         tabWidth: Binding<Int>,
+        indentOption: Binding<IndentOption> = .constant(.spaces(count: 4)),
         lineHeight: Binding<Double>,
         wrapLines: Binding<Bool>,
         editorOverscroll: Binding<Double> = .constant(0.0),
@@ -48,6 +50,7 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
         self.useThemeBackground = useThemeBackground
         self._font = font
         self._tabWidth = tabWidth
+        self._indentOption = indentOption
         self._lineHeight = lineHeight
         self._wrapLines = wrapLines
         self._editorOverscroll = editorOverscroll
@@ -62,6 +65,7 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
     @Binding private var theme: EditorTheme
     @Binding private var font: NSFont
     @Binding private var tabWidth: Int
+    @Binding private var indentOption: IndentOption
     @Binding private var lineHeight: Double
     @Binding private var wrapLines: Bool
     @Binding private var editorOverscroll: Double
@@ -80,6 +84,7 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
             font: font,
             theme: theme,
             tabWidth: tabWidth,
+            indentOption: indentOption,
             wrapLines: wrapLines,
             cursorPosition: $cursorPosition,
             editorOverscroll: editorOverscroll,
@@ -94,19 +99,24 @@ public struct CodeEditTextView: NSViewControllerRepresentable {
 
     public func updateNSViewController(_ controller: NSViewControllerType, context: Context) {
         controller.font = font
-        controller.tabWidth = tabWidth
         controller.wrapLines = wrapLines
         controller.useThemeBackground = useThemeBackground
         controller.lineHeightMultiple = lineHeight
         controller.editorOverscroll = editorOverscroll
         controller.contentInsets = contentInsets
 
-        // Updating the language and theme needlessly can cause highlights to be re-calculated.
+        // Updating the language, theme, tab width and indent option needlessly can cause highlights to be re-calculated
         if controller.language.id != language.id {
             controller.language = language
         }
         if controller.theme != theme {
             controller.theme = theme
+        }
+        if controller.indentOption != indentOption {
+            controller.indentOption = indentOption
+        }
+        if controller.tabWidth != tabWidth {
+            controller.tabWidth = tabWidth
         }
 
         controller.reloadUI()
--- Sources/CodeEditTextView/Filters/TabReplacementFilter.swift
+++ Sources/CodeEditTextView/Filters/TabReplacementFilter.swift
@@ -12,11 +12,11 @@ import TextStory
 /// Filter for replacing tab characters with the user-defined indentation unit.
 /// - Note: The undentation unit can be another tab character, this is merely a point at which this can be configured.
 struct TabReplacementFilter: Filter {
-    let indentationUnit: String
+    let indentOption: IndentOption
 
     func processMutation(_ mutation: TextMutation, in interface: TextInterface) -> FilterAction {
-        if mutation.string == "\t" {
-            interface.applyMutation(TextMutation(insert: indentationUnit,
+        if mutation.string == "\t" && indentOption != .tab && mutation.delta > 0 {
+            interface.applyMutation(TextMutation(insert: indentOption.stringValue,
                                                  at: mutation.range.location,
                                                  limit: mutation.limit))
             return .discard
