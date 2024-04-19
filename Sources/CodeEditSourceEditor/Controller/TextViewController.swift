2498247f302eb00ba29a0701c5dd7333dd753afc
sophiahooley <sophia.hooley@vanderbilt.edu>
2024-04-19 09:17:32 -0500

    print line info

--- Sources/CodeEditSourceEditor/Controller/TextViewController.swift
+++ Sources/CodeEditSourceEditor/Controller/TextViewController.swift
@@ -46,8 +46,8 @@ public class TextViewController: NSViewController {
         if let cursorPosition = cursorPositions.first {
             print(textView.layoutManager.textLineForIndex(cursorPosition.line - 1) ?? 0)
             if let lineInfo = textView.layoutManager.textLineForIndex(cursorPosition.line - 1) {
-                print(lineInfo.data.lineFragments)
                 let lineFirstCharIndex = lineInfo.range.location
+                print(textView.layoutManager.textLineForOffset(lineFirstCharIndex))
                 // let currentLinePosition = textView.layoutManager.lineStorage.getLine(atIndex: lineFirstCharIndex)
                 let languageCommentStr = language.lineCommentString
                 // let lengthOfCommentStr = languageCommentStr.count
