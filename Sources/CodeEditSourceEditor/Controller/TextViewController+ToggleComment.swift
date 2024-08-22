347b09227262e77df427426b92c98d35fc3b2898
Tom Ludwig <tommludwig@icloud.com>
2024-08-22 09:45:54 +0200

    Improve readability

--- Sources/CodeEditSourceEditor/Controller/TextViewController+ToggleComment.swift
+++ Sources/CodeEditSourceEditor/Controller/TextViewController+ToggleComment.swift
@@ -11,16 +11,16 @@ import AppKit
 extension TextViewController {
     /// Method called when CMD + / key sequence recognized, comments cursor's current line of code
     public func commandSlashCalled() {
-        guard let cursorPosition  = cursorPositions.first else { return }
-        let lineNumbers = lineNumbersFromRange(for: cursorPosition.range, in: text)
+        guard let cursorPosition = cursorPositions.first else { return }
+        let lineNumbers = lineNumbers(from: cursorPosition.range, in: text)
 
         for lineNumber in lineNumbers {
-            commentLine(line: lineNumber)
+            toggleComment(on: lineNumber)
         }
     }
 
     // Calculate the start and end line numbers for a given range
-    func lineNumbersFromRange(for range: NSRange, in text: String) -> [Int] {
+    func lineNumbers(from range: NSRange, in text: String) -> [Int] {
         let startLine = lineNumber(at: range.location, in: text)
         let endLine = lineNumber(at: NSMaxRange(range), in: text)
         return Array(startLine...endLine)
@@ -33,30 +33,30 @@ extension TextViewController {
         return substring.components(separatedBy: "\n").count
     }
 
-    private func commentLine(line: Int) {
+    private func toggleComment(on line: Int) {
         // Many languages require a character sequence at the beginning of the line to comment the line.
         // (ex. python #, C++ //)
         // If such a sequence exists, we will insert that sequence at the beginning of the line
         if !language.lineCommentString.isEmpty {
-            toggleCharsAtBeginningOfLine(chars: language.lineCommentString, lineNumber: line)
+            toggleCharsAtBeginningOfLine(chars: language.lineCommentString, for: line)
         } else {
             // In other cases, languages require a character sequence 
             // at beginning and end of a line, aka a range comment (Ex. HTML <!--line here -->)
             // We treat the line as a one-line range to comment it
             // out using rangeCommentStrings on both sides of the line
             let (openComment, closeComment) = language.rangeCommentStrings
-            toggleCharsAtEndOfLine(chars: closeComment, lineNumber: line)
-            toggleCharsAtBeginningOfLine(chars: openComment, lineNumber: line)
+            toggleCharsAtEndOfLine(chars: closeComment, for: line)
+            toggleCharsAtBeginningOfLine(chars: openComment, for: line)
         }
     }
 
     ///  Toggles comment string at the beginning of a specified line (lineNumber is 1-indexed)
-    private func toggleCharsAtBeginningOfLine(chars: String, lineNumber: Int) {
+    private func toggleCharsAtBeginningOfLine(chars: String, for lineNumber: Int) {
         guard let lineInfo = textView.layoutManager.textLineForIndex(lineNumber - 1),
               let lineString = textView.textStorage.substring(from: lineInfo.range) else {
             return
         }
-        let firstNonWhiteSpaceCharIndex = lineString.firstIndex(where: {!$0.isWhitespace}) ?? lineString.startIndex
+        let firstNonWhiteSpaceCharIndex = lineString.firstIndex(where: { !$0.isWhitespace }) ?? lineString.startIndex
         let numWhitespaceChars = lineString.distance(from: lineString.startIndex, to: firstNonWhiteSpaceCharIndex)
         let firstCharsInLine = lineString.suffix(from: firstNonWhiteSpaceCharIndex).prefix(chars.count)
         // toggle comment off
@@ -75,7 +75,7 @@ extension TextViewController {
     }
 
     ///  Toggles a specific string of characters at the end of a specified line. (lineNumber is 1-indexed)
-    private func toggleCharsAtEndOfLine(chars: String, lineNumber: Int) {
+    private func toggleCharsAtEndOfLine(chars: String, for lineNumber: Int) {
         guard let lineInfo = textView.layoutManager.textLineForIndex(lineNumber - 1), !lineInfo.range.isEmpty else {
             return
         }
