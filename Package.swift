e6b0098d5040ad008af7c0e7694e4491c0adade6
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2024-08-24 10:36:33 -0500

    Bump CETV

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // A fast, efficient, text view for code.
         .package(
             url: "https://github.com/CodeEditApp/CodeEditTextView.git",
-            from: "0.7.5"
+            from: "0.7.6"
         ),
         // tree-sitter languages
         .package(
