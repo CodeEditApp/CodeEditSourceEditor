53605e45d5873377d4681f326aa1c0d2a39589df
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2024-11-11 20:43:48 -0600

    Slight Doc Rewrite

--- Sources/CodeEditSourceEditor/Enums/CaptureModifier.swift
+++ Sources/CodeEditSourceEditor/Enums/CaptureModifier.swift
@@ -115,13 +115,13 @@ public struct CaptureModifierSet: OptionSet, Equatable, Hashable, Sendable {
         var rawValue = self.rawValue
 
         // This set is represented by an integer, where each `1` in the binary number represents a value.
-        // We can treat the index of the `1` as the raw value of a ``CaptureModifier`` (the index in 0b0100 would be 2).
-        // This loops through each `1` in the `rawValue`, finds the represented modifier, and 0's out the `1` so we can
-        // get the next one using the binary & operator (0b0110 -> 0b0100 -> 0b0000 -> finish).
+        // We can interpret the index of the `1` as the raw value of a ``CaptureModifier`` (the index in 0b0100 would
+        // be 2). This loops through each `1` in the `rawValue`, finds the represented modifier, and 0's out the `1` so
+        // we can get the next one using the binary & operator (0b0110 -> 0b0100 -> 0b0000 -> finish).
         var values: [Int8] = []
         while rawValue > 0 {
             values.append(Int8(rawValue.trailingZeroBitCount))
-            // Clears the bit at the desired index (0b011 & 0b110 = 0b010 if clearing index 0)
+            // Clears the bit at the desired index (eg: 0b110 if clearing index 0)
             rawValue &= ~UInt(1 << rawValue.trailingZeroBitCount)
         }
         return values.compactMap({ CaptureModifier(rawValue: $0) })
