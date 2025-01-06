85b6bf5f963f4a750d6784028e48b9fbe04e4769
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-01-06 12:31:16 -0600

    Add A Test

--- Tests/CodeEditSourceEditorTests/Highlighting/HighlighterTests.swift
+++ Tests/CodeEditSourceEditorTests/Highlighting/HighlighterTests.swift
@@ -38,12 +38,30 @@ final class HighlighterTests: XCTestCase {
         func attributesFor(_ capture: CaptureName?) -> [NSAttributedString.Key: Any] { [:] }
     }
 
+    class SentryStorageDelegate: NSObject, NSTextStorageDelegate {
+        var editedIndices: IndexSet = IndexSet()
+
+        func textStorage(
+            _ textStorage: NSTextStorage,
+            didProcessEditing editedMask: NSTextStorageEditActions,
+            range editedRange: NSRange,
+            changeInLength delta: Int) {
+                editedIndices.insert(integersIn: editedRange)
+            }
+    }
+
+    var attributeProvider: MockAttributeProvider!
+    var textView: TextView!
+
+    override func setUp() {
+        attributeProvider = MockAttributeProvider()
+        textView = Mock.textView()
+        textView.frame = NSRect(x: 0, y: 0, width: 1000, height: 1000)
+    }
+
     @MainActor
     func test_canceledHighlightsAreInvalidated() {
         let highlightProvider = MockHighlightProvider()
-        let attributeProvider = MockAttributeProvider()
-        let textView = Mock.textView()
-        textView.frame = NSRect(x: 0, y: 0, width: 1000, height: 1000)
         textView.setText("Hello World!")
         let highlighter = Mock.highlighter(
             textView: textView,
@@ -62,23 +80,8 @@ final class HighlighterTests: XCTestCase {
 
     @MainActor
     func test_highlightsDoNotInvalidateEntireTextView() {
-        class SentryStorageDelegate: NSObject, NSTextStorageDelegate {
-            var editedIndices: IndexSet = IndexSet()
-
-            func textStorage(
-                _ textStorage: NSTextStorage,
-                didProcessEditing editedMask: NSTextStorageEditActions,
-                range editedRange: NSRange,
-                changeInLength delta: Int) {
-                editedIndices.insert(integersIn: editedRange)
-            }
-        }
-
         let highlightProvider = TreeSitterClient()
         highlightProvider.forceSyncOperation = true
-        let attributeProvider = MockAttributeProvider()
-        let textView = Mock.textView()
-        textView.frame = NSRect(x: 0, y: 0, width: 1000, height: 1000)
         textView.setText("func helloWorld() {\n\tprint(\"Hello World!\")\n}")
 
         let highlighter = Mock.highlighter(
@@ -96,5 +99,50 @@ final class HighlighterTests: XCTestCase {
         highlighter.invalidate(invalidSet) // Invalidate first line
 
         XCTAssertEqual(sentryStorage.editedIndices, invalidSet) // Should only cause highlights on the first line
+    }
+
+    // This test isn't testing much highlighter functionality. However, we've seen crashes and other errors after normal
+    // editing that were caused by the highlighter and would only have been caught by an integration test like this.
+    @MainActor
+    func test_editFile() {
+        let highlightProvider = TreeSitterClient()
+        highlightProvider.forceSyncOperation = true
+        textView.setText("func helloWorld() {\n\tprint(\"Hello World!\")\n}") // 44 chars
+
+        let highlighter = Mock.highlighter(
+            textView: textView,
+            highlightProvider: highlightProvider,
+            attributeProvider: attributeProvider
+        )
+        textView.addStorageDelegate(highlighter)
+        highlighter.setLanguage(language: .swift)
+        highlighter.invalidate()
+
+        // Delete Characters
+        textView.replaceCharacters(in: [NSRange(location: 43, length: 1)], with: "")
+        textView.replaceCharacters(in: [NSRange(location: 0, length: 4)], with: "")
+        textView.replaceCharacters(in: [NSRange(location: 6, length: 5)], with: "")
+        textView.replaceCharacters(in: [NSRange(location: 25, length: 5)], with: "")
+
+        XCTAssertEqual(textView.string, " hello() {\n\tprint(\"Hello !\")\n")
+
+        // Insert Characters
+        textView.replaceCharacters(in: [NSRange(location: 29, length: 0)], with: "}")
+        textView.replaceCharacters(
+            in: [NSRange(location: 25, length: 0), NSRange(location: 6, length: 0)],
+            with: "World"
+        )
+        // emulate typing with a cursor
+        textView.selectionManager.setSelectedRange(NSRange(location: 0, length: 0))
+        textView.insertText("f")
+        textView.insertText("u")
+        textView.insertText("n")
+        textView.insertText("c")
+        XCTAssertEqual(textView.string, "func helloWorld() {\n\tprint(\"Hello World!\")\n}")
+
+        // Replace contents
+        textView.replaceCharacters(in: textView.documentRange, with: "")
+        textView.insertText("func helloWorld() {\n\tprint(\"Hello World!\")\n}")
+        XCTAssertEqual(textView.string, "func helloWorld() {\n\tprint(\"Hello World!\")\n}")
     }
 }
