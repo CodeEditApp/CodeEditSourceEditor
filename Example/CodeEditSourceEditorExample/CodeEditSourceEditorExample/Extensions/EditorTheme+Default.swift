b5b5a8f8d13605ae8b94e147965686382482d9d6
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-01-10 14:59:35 -0600

    Add Bold and Italics Support To Themes

--- Example/CodeEditSourceEditorExample/CodeEditSourceEditorExample/Extensions/EditorTheme+Default.swift
+++ Example/CodeEditSourceEditorExample/CodeEditSourceEditorExample/Extensions/EditorTheme+Default.swift
@@ -12,22 +12,22 @@ import CodeEditSourceEditor
 extension EditorTheme {
     static var standard: EditorTheme {
         EditorTheme(
-            text: .init(hex: "000000"),
-            insertionPoint: .init(hex: "000000"),
-            invisibles: .init(hex: "D6D6D6"),
-            background: .init(hex: "FFFFFF"),
-            lineHighlight: .init(hex: "ECF5FF"),
-            selection: .init(hex: "B2D7FF"),
-            keywords: .init(hex: "9B2393"),
-            commands: .init(hex: "326D74"),
-            types: .init(hex: "0B4F79"),
-            attributes: .init(hex: "815F03"),
-            variables: .init(hex: "0F68A0"),
-            values: .init(hex: "6C36A9"),
-            numbers: .init(hex: "1C00CF"),
-            strings: .init(hex: "C41A16"),
-            characters: .init(hex: "1C00CF"),
-            comments: .init(hex: "267507")
+            text: Attribute(color: NSColor(hex: "000000")),
+            insertionPoint: NSColor(hex: "000000"),
+            invisibles: Attribute(color: NSColor(hex: "D6D6D6")),
+            background: NSColor(hex: "FFFFFF"),
+            lineHighlight: NSColor(hex: "ECF5FF"),
+            selection: NSColor(hex: "B2D7FF"),
+            keywords: Attribute(color: NSColor(hex: "9B2393"), bold: true),
+            commands: Attribute(color: NSColor(hex: "326D74")),
+            types: Attribute(color: NSColor(hex: "0B4F79")),
+            attributes: Attribute(color: NSColor(hex: "815F03")),
+            variables: Attribute(color: NSColor(hex: "0F68A0")),
+            values: Attribute(color: NSColor(hex: "6C36A9")),
+            numbers: Attribute(color: NSColor(hex: "1C00CF")),
+            strings: Attribute(color: NSColor(hex: "C41A16"), bold: true, italic: true),
+            characters: Attribute(color: NSColor(hex: "1C00CF")),
+            comments: Attribute(color: NSColor(hex: "267507"), italic: true)
         )
     }
 }
