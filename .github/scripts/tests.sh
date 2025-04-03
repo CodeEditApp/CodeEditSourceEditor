f74d2e899c80c9bc9ec69ead1b2d6f113feed990
Austin Condiff <austin.condiff@gmail.com>
2025-04-03 09:41:05 -0500

    Update tests.sh

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ARCH=""
-
-if [ "$1" = "arm" ]; then
+    
+if [ $1 = "arm" ]
+then
     ARCH="arm64"
 else
     ARCH="x86_64"
@@ -12,18 +13,9 @@ echo "Building with arch: ${ARCH}"
 
 export LC_CTYPE=en_US.UTF-8
 
-DEVICE_ID=$(xcrun xctrace list devices 2>/dev/null | grep -m1 "My Mac" | grep "${ARCH}" | awk -F '[()]' '{print $2}')
-
-if [ -z "$DEVICE_ID" ]; then
-  echo "Failed to find device ID for arch ${ARCH}"
-  exit 1
-fi
-
-echo "Using device ID: $DEVICE_ID"
-
 set -o pipefail && arch -"${ARCH}" xcodebuild  \
            -scheme CodeEditSourceEditor \
            -derivedDataPath ".build" \
-           -destination "id=${DEVICE_ID}" \
+           -destination "platform=macos,arch=${ARCH}" \
            -skipPackagePluginValidation \
            clean test | xcpretty
