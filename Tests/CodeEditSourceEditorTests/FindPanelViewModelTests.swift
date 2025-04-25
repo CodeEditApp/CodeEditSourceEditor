35df1a218a1ada5f4359afd7a8d5f8e844497c74
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-04-25 14:16:02 -0500

    Fix Fialing Test

---
+++ Tests/CodeEditSourceEditorTests/FindPanelViewModelTests.swift
@@ -0,0 +1,40 @@
+//
+//  FindPanelViewModelTests.swift
+//  CodeEditSourceEditor
+//
+//  Created by Khan Winter on 4/25/25.
+//
+
+import Testing
+import AppKit
+import CodeEditTextView
+@testable import CodeEditSourceEditor
+
+@MainActor
+struct FindPanelViewModelTests {
+    class MockPanelTarget: FindPanelTarget {
+        var emphasisManager: EmphasisManager?
+        var text: String = ""
+        var findPanelTargetView: NSView
+        var cursorPositions: [CursorPosition] = []
+
+        @MainActor init() {
+            findPanelTargetView = NSView()
+        }
+
+        func setCursorPositions(_ positions: [CursorPosition], scrollToVisible: Bool) { }
+        func updateCursorPosition() { }
+        func findPanelWillShow(panelHeight: CGFloat) { }
+        func findPanelWillHide(panelHeight: CGFloat) { }
+        func findPanelModeDidChange(to mode: FindPanelMode) { }
+    }
+
+    @Test func viewModelHeightUpdates() async throws {
+        let model = FindPanelViewModel(target: MockPanelTarget())
+        model.mode = .find
+        #expect(model.panelHeight == 28)
+
+        model.mode = .replace
+        #expect(model.panelHeight == 54)
+    }
+}
