d23caf62cfa9c93eebb1e0b3b5cdc864b100f746
Austin Condiff <austin.condiff@gmail.com>
2025-05-02 11:13:58 -0500

    Made find panel responsive to width. When too narrow control text turns to icons.

---
+++ Sources/CodeEditSourceEditor/Find/PanelView/FindPanelContent.swift
@@ -0,0 +1,35 @@
+struct FindPanelContent: View {
+    @ObservedObject var viewModel: FindPanelViewModel
+    @FocusState.Binding var focus: FindPanelView.FindPanelFocus?
+    var findModePickerWidth: Binding<CGFloat>
+    var condensed: Bool
+
+    var body: some View {
+        HStack(spacing: 5) {
+            VStack(alignment: .leading, spacing: 4) {
+                FindSearchField(
+                    viewModel: viewModel,
+                    focus: $focus,
+                    findModePickerWidth: findModePickerWidth,
+                    condensed: condensed
+                )
+                if viewModel.mode == .replace {
+                    ReplaceSearchField(
+                        viewModel: viewModel,
+                        focus: $focus,
+                        findModePickerWidth: findModePickerWidth,
+                        condensed: condensed
+                    )
+                }
+            }
+            VStack(alignment: .leading, spacing: 4) {
+                FindControls(viewModel: viewModel, condensed: condensed)
+                if viewModel.mode == .replace {
+                    Spacer(minLength: 0)
+                    ReplaceControls(viewModel: viewModel, condensed: condensed)
+                }
+            }
+            .fixedSize()
+        }
+    }
+}
