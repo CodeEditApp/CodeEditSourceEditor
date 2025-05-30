6549199a2160b8e070e01720a7e1c9105721f720
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-05-30 13:12:02 -0500

    Update Example App Storage

--- Example/CodeEditSourceEditorExample/CodeEditSourceEditorExample/Views/ContentView.swift
+++ Example/CodeEditSourceEditorExample/CodeEditSourceEditorExample/Views/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { proxy in
             CodeEditSourceEditor(
-                $document.text,
+                document.text,
                 language: language,
                 theme: theme,
                 font: font,
