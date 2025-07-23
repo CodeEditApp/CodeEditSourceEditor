e9136b4406b20d4c69abd8d28ad05fb1130c473d
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-07-23 13:28:27 -0500

    Update CodeEditTextView to `0.12.0` (#347)
    Updates CodeEditTextView to `0.12.0`

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // A fast, efficient, text view for code.
         .package(
             url: "https://github.com/CodeEditApp/CodeEditTextView.git",
-            from: "0.11.4"
+            from: "0.12.0"
         ),
         // tree-sitter languages
         .package(
